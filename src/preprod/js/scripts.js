50b2e7989ca69e0b60fdbad38f29072bed85cfd5
Skidan <dmytro.skydanenko@gmail.com>
2019-02-19 15:21:31 +0200

    vue reactions

--- src/preprod/js/scripts.js
+++ src/preprod/js/scripts.js
@@ -6,8 +6,9 @@ Vue.component('trailers', {
 var trailerData = new Vue({
 	el: "#wrapper",
 	data: {
+		//variables
 		trailerTypes: [
-			{id: 0, name: "-select-"},
+			{id: 0, name: "- select type -"},
 			{id: 1, name: "SPC14"},
 			{id: 2, name: "SPC16"},
 			{id: 3, name: "GPP23"},
@@ -16,7 +17,34 @@ var trailerData = new Vue({
 			{id: 6, name: "SPC30"},
 			{id: 7, name: "SPE16"}
 		],
-		//variables
+		currentTrailer: [
+			{name: "XXXXX"},
+			{category: "XXX"},
+			{variant: "XXX"},
+			{version: "-"},
+			{year: "0000"},
+			{mass: 1234},
+			{payload: 12345},
+			{techPayload: 123456},
+			{homologation: "e32*167/2013*00000*00"},
+			{vin: "UMEXXX00XX0000000"},
+			{country: "XX"},
+			{onroad: [
+				{whole: 23123},
+				{p0: 3000},
+				{p1: 10000},
+				{p2: 10000},
+				{p3: 10000}
+			]},
+			{offroad: [
+				{whole: 53123},
+				{p0: 4000},
+				{p1: 14000},
+				{p2: 14000},
+				{p3: 14000}
+			]}
+		]
+		
 	},
 	methods: {
 		//methods
